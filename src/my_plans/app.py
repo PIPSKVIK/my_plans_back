55e4e231988fb7bec901c49edfe98f5c0737bdb3
Alexander <milovanska@gmail.com>
2023-01-20 06:19:33 +0300

    feat(dev): add corse middleware in app

--- src/my_plans/app.py
+++ src/my_plans/app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api import router
+
+origins = ["*"]
 
 app = FastAPI(
     title='My Plans'
 )
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
